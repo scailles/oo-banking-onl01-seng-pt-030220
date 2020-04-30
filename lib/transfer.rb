12b41e0e055e952d28671dd47ef205e83b8dd7f2
Shaina C <scailles23@gmail.com>
2020-04-30 04:47:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
     def execute_transaction 
-    if valid? && status == "pending"
+    if valid? && status == "pending" && sender.balance > amount
         sender.balance -= amount
         receiver.balance += amount
         self.status = "complete"
