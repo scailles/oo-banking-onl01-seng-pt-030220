b51a2e356a722998f72012859bea1aeac10e693d
Shaina C <scailles23@gmail.com>
2020-04-30 04:32:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
     if @sender.valid? && self.status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
+      self.status = "complete"
     else
       self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
